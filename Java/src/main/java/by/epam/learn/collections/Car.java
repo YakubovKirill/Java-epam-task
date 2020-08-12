59af685f086ae5883627bb1fb69e525467e34314
Kirill <yakubov.kirill.work@gmail.com>
2020-08-12 11:40:29 +0300

    Create collections and refactor folder

---
+++ Java/src/main/java/by/epam/learn/collections/Car.java
@@ -0,0 +1,69 @@
+package by.epam.learn.collections;
+
+public class Car {
+    private int id;
+    private int carCost;
+    private double fuelRate;
+    public String carName;
+    public int maxSpeed;
+    public int numberOfSeats;
+
+    public Car() {}
+
+    public Car(int id, String carName, int maxSpeed, int numberOfSeats, double fuelRate, int carCost) {
+        this.id = id;
+        this.carName = carName;
+        this.maxSpeed = maxSpeed;
+        this.numberOfSeats = numberOfSeats;
+        this.fuelRate = fuelRate;
+        this.carCost = carCost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getFuelRate() {
+        return fuelRate;
+    }
+
+    public int getCarCost() {
+        return carCost;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCar {" +
+                "id=" + id +
+                ", carCost=" + carCost +
+                ", fuelRate=" + fuelRate +
+                ", carName='" + carName + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", numberOfSeats=" + numberOfSeats +
+                '}';
+    }
+}
